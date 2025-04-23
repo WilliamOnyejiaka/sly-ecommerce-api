c0104f515a9862f469d72e72316e819c45c42bda
WilliamOnyejiaka <williamonyejiaka08062528003@gmail.com>
2025-04-23 15:17:03 +0100

    - handled a little bug

--- src/facade/StoreManagementFacade.ts
+++ src/facade/StoreManagementFacade.ts
@@ -1,6 +1,7 @@
 import BaseFacade from "./bases/BaseFacade";
 import { ImageService, Store } from "../services";
 import { StoreDetailsDto } from "../types/dtos";
+import { UserType } from "../types/enums";
 
 
 export default class StoreManagementFacade extends BaseFacade {
@@ -8,11 +9,11 @@ export default class StoreManagementFacade extends BaseFacade {
     public readonly storeService: Store = new Store();
     private readonly imageService: ImageService = new ImageService();
 
-    public async createStoreAll(storeDetailsDto: StoreDetailsDto,images: Express.Multer.File[]){
-        return await this.storeService.createStoreAll(storeDetailsDto,images);
+    public async createStoreAll(storeDetailsDto: StoreDetailsDto, images: Express.Multer.File[], userType: UserType) {
+        return await this.storeService.createStoreAll(storeDetailsDto, images, userType);
     }
 
-    public async createStore(storeDetailsDto: StoreDetailsDto){
+    public async createStore(storeDetailsDto: StoreDetailsDto) {
         return await this.storeService.createStore(storeDetailsDto);
     }
 
